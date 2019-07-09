4f5b07261be3046920d682fd08bfca76c8175730
Matthew Hoang <hoangmatthew1999@gmail.com>
2019-07-08 18:57:29 -0700

    added a border

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const fs = require('fs')
 
-const styles = {borderWidth: "400px", background: "green" };
+const styles = {borderWidth: "400px", background: "green", width: "400px" };
 
 function App() {
   return (
     <div className="App">
-      <div styles = {{styles}}>
+      <div style = {{styles}}>
         <button>New Quote</button>
       </div>
     </div>
   );
 }
-
+/*
+function BuildingQuotes(){
+  fs.readFile('quotes.txt', (err, data) => { 
+    if (err) throw err; 
+  
+    // Converting Raw Buffer to text 
+    // data using tostring function. 
+    );
+}
+*/
 export default App;
