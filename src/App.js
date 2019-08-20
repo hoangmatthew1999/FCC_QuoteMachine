43cb9649751f4ff63d104e9ac20d403ee0a71bd2
Matthew Hoang <hoangmatthew1999@gmail.com>
2019-08-19 20:16:01 -0700

    started to work on the color aspect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SSL_OP_TLS_ROLLBACK_BUG } from 'constants';
 const fs = require('fs')
+const path = require('path')
+const fs_ = require('./fs.js')
 
-const styles = {borderWidth: "400px", background: "green", width: "400px" };
-
-function App() {
-  return (
-    <div className="App">
-      <div style = {{styles}}>
-        <button>New Quote</button>
-      </div>
-    </div>
-  );
-}
+const styles = {borderWidth: "400px", background: "green", width: "400px", borderHeight: "400px" };
 /*
-function BuildingQuotes(){
-  fs.readFile('quotes.txt', (err, data) => { 
-    if (err) throw err; 
-  
-    // Converting Raw Buffer to text 
-    // data using tostring function. 
+function App() {
+  const colors = ['red','blue','green','magenta','pink','purple',]
+    return (
+      <div className = "background">
+        <div id = "app" className="App" style = { {background: this.state.color} }>
+          <button onClick = {this.randomColor}>New Quote</button>
+        </div>
+      </div>
     );
 }
+function changingColor(){
+  console.log('hi');
+  const a = document.querySelector("#app");
+  console.log(a);
+}
+function BuildingQuotes(){
+  fs.readFile( 
+    path.join(__dirname, '../output.txt'),{encoding: 'utf-8' },
+function(error,data){if(error)
+    return console.error(error)
+    console.log(data)} 
+)
+}
 */
-export default App;
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "red",
+    };
+  }
+  changeColor = () => {
+    this.setState({color: "blue"});
+  }
+  render() {
+    return (
+        <div className = "background">
+        <div id = "app" className="App" style = { {background: this.state.color} }>
+          <button onClick = {this.changeColor}>New Quote</button>
+        </div>
+      </div>
+    );
+  }
+}
+export default Car;
